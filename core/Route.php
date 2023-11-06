8a37b877d76b289cf9d562fb41dd54578d700dd8
ElmarAliyev-7 <liyevelmar02@gmail.com>
2023-11-06 15:10:58 +0400

    get method

--- core/Route.php
+++ core/Route.php
@@ -1,12 +1,18 @@
 <?php
 
-namespace core;
+namespace Core;
 
 class Route
 {
-    public static function get()
+    public static function get(string $uri, array $controllerMethod)
     {
-
+        if($_SERVER['REQUEST_URI'] === $uri) {
+            $controller = $controllerMethod[0];
+            $method = $controllerMethod[1];
+            $newController = new $controller;
+            return $newController->$method();
+        }
+       return header("http://127.0.0.1:8000/404", true, 404);
     }
 
     public static function post()
